30e2db3386bb1039669d7c848e03e85ef8864d4a
Bryan Gomes <bryanpgomes@gmail.com>
2023-08-16 20:45:16 -0400

    I broke some stuff

--- routes/users-api.js
+++ routes/users-api.js
@@ -12,7 +12,7 @@ const userQueries = require('../db/queries/users');
 router.get('/', (req, res) => {
   userQueries.getUsers()
     .then(users => {
-      res.json({ users });
+      console.log({ users });
     })
     .catch(err => {
       res
